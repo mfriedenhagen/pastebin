783960274871720a050b99a40114fe3a0ebafb8b
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-05-28 20:48:35 +0200

    Init with surefire-2.14.1

---
+++ src/test/java/net/friedenhagen/surefireencodingtest/EncodingTest.java
@@ -0,0 +1,14 @@
+package net.friedenhagen.surefireencodingtest;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Mirko Friedenhagen
+ */
+public class EncodingTest {
+    @Test
+    public void throwAssumption() {
+        
+    }
+}
