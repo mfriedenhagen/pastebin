ce18d6df1dbd7b9b500d71f44a792c6925ad8111
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-06-17 21:40:25 +0200

    Print defaultCharset picked up from CLI.

--- src/test/java/net/friedenhagen/surefireencodingtest/EncodingTest.java
+++ src/test/java/net/friedenhagen/surefireencodingtest/EncodingTest.java
@@ -1,5 +1,6 @@
 package net.friedenhagen.surefireencodingtest;
 
+import java.nio.charset.Charset;
 import static org.junit.Assume.assumeNoException;
 import org.junit.Test;
 
@@ -12,6 +13,7 @@ public class EncodingTest {
     public void throwAssumption() {
         final IllegalStateException exception = new IllegalStateException("äöüß");
         System.out.println(exception);
+        System.out.println(Charset.defaultCharset());
         assumeNoException("&auml;&ouml;&uuml;&szlig;", exception);
     }
 }
